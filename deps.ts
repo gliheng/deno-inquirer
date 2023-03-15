cd8487c9d00e18369af0d15efe256c8323f64cde
Geoffrey Flarity <geoff.flarity@gmail.com>
2023-03-15 13:36:17 -0400

    use the latest keypress module, Deno.setRaw has been moved/replaced

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { readKeypress } from 'https://deno.land/x/keypress@0.0.4/mod.ts';
+export { readKeypress } from 'https://deno.land/x/keypress@0.0.10/mod.ts';
 export * as color from 'https://deno.land/x/nanocolors@0.1.12/mod.ts';
 export * as ansi from 'https://deno.land/x/ansi@1.0.1/mod.ts';
 
